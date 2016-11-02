45d93fb8dd67cca4c037bb6d6f733382e53691eb
rachel00 <rachel.cabrales.simontech@gmail.com>
2016-11-02 15:31:23 +0000

    First saved version of my nitrous code.

---
+++ index.js
@@ -0,0 +1,33 @@
+var add = function(a, b) {
+  return a + b;
+};
+var subtract = function(a, b) {
+  return a - b;
+};
+var multiply = function(a, b) {
+  return a * b;
+};
+var divide = function(a, b) {
+  return a / b;
+};
+var inc = function(number){
+  return number + 1
+};
+var dec = function(number){
+  return number - 1
+};
+
+var makeInt = function(string){
+  return parseInt(string, 10);
+};
+
+var preserveDecimal = function(string){
+  return parseFloat(string);
+};
+
+
+
+
+
+
+
